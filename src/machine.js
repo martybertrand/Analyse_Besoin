77bf5709ca537385ee46d893856942633c1a4954
martybertrand <91955140+martybertrand@users.noreply.github.com>
2023-01-03 09:32:42 +0100

    gestion des stocks

--- src/machine.js
+++ src/machine.js
@@ -1,17 +1,26 @@
 export default class Machine {
 
     constructor() {
-        this.cafeServis = 0;
-        this.argentEncaisse = 0;
+        this.cafeServis = 0
+        this.argentEncaisse = 0
         this.eauDisponible = true
         this.gobeletDisponible = true
         this.cafeEnStock = true
+
+        this.nbDoseCafe = 1
+        this.nbGobelet = 1
     }
     
     Insertion(montant) {
-        if(montant >= 0.4 && this.eauDisponible && this.gobeletDisponible && this.cafeEnStock) {
-            this.cafeServis ++
-            this.argentEncaisse += montant
+        if(montant >= 0.4 &&
+            this.eauDisponible &&
+            this.gobeletDisponible &&
+            this.cafeEnStock &&
+            this.nbDoseCafe > 0 &&
+            this.nbGobelet > 0) {
+                this.cafeServis ++
+                this.argentEncaisse += montant
+                this.nbGobelet --
         }
     }
 
@@ -33,5 +42,23 @@ export default class Machine {
 
     CafeEpuise() {
         this.cafeEnStock = false
+    }
+
+
+
+    SansGobelets() {
+        return AyantXGobelet(0)
+    }
+
+    SansCafe() {
+        return this.AyantDosesDeCafe(0)
+    }
+
+    AyantXGobelet(x) {
+        this.nbGobelet = x        
+    }
+
+    AyantDosesDeCafe(y) {
+        this.nbDoseCafe = y
     }
 }
--- test/stocks.test.js
+++ test/stocks.test.js
@@ -4,21 +4,46 @@ describe('Stocks', () => {
     test('ETANT DONNE une machine ayant un seul gobelet QUAND on met deux fois 40 cts ALORS seul un café coule ET l\'argent du second café est rendu', () => {
         //ETANT DONNE une machine ayant un seul gobelet
         const machine = new Machine()
-        machine.CouperEau()
+        machine.AyantXGobelet(1)
 
         let nbCafeInit = machine.GetNombreCafésServis()
         let argentInit = machine.GetArgentEncaisse()
+        const somme = 0.40
 
         //QUAND on met deux fois 40cts
-        machine.Insertion(0.40)
+        machine.Insertion(somme)
+        machine.Insertion(somme)
 
         //ALORS seul un café coule
         const nbCafeFinal = machine.GetNombreCafésServis()
-        expect(nbCafeFinal).toBe(nbCafeInit)
+        expect(nbCafeFinal).toBe(nbCafeInit + 1)
 
-        //ET l'argent du second est rendu
+        //ET l'argent du second  rendu
         const argentFinal = machine.GetArgentEncaisse()
-        expect(argentFinal).toBe(argentInit)
+        expect(argentFinal).toBe(argentInit + somme)
+        
+    });
+
+    test('ETANT DONNE une machine ayant une seule dose de café QUAND on met deux fois 40 cts ALORS seul un café coule ET l\'argent du second café est rendu', () => {
+        //ETANT DONNE une machine ayant une seule dose de café
+        const machine = new Machine()
+        machine.AyantDosesDeCafe(1)
+
+        let nbCafeInit = machine.GetNombreCafésServis()
+        let argentInit = machine.GetArgentEncaisse()
+        const somme = 0.40
+
+        //QUAND on met deux fois 40cts
+        machine.Insertion(somme)
+        machine.Insertion(somme)
+
+        //ALORS seul un café coule
+        const nbCafeFinal = machine.GetNombreCafésServis()
+        expect(nbCafeFinal).toBe(nbCafeInit + 1)
+
+        //ET l'argent du second  rendu
+        const argentFinal = machine.GetArgentEncaisse()
+        expect(argentFinal).toBe(argentInit + somme)
         
     }); 
 });
