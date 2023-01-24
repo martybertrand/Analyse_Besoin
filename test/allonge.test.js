e85c2559e0a2a22b9a6b7d84f0231e7c7cf462e3
CalvinC44 <calel44jke@gmail.com>
2023-01-24 10:31:55 +0100

    ajout test allonge

---
+++ test/allonge.test.js
@@ -0,0 +1,83 @@
+import Machine from "../src/machine";
+
+describe("Allonge", () => {
+	test("Machine bouton allonge, en stock, bien payé", () => {
+		//ETANT DONNE une machine à café où on appuie sur le bouton allonge
+		const machine = new Machine();
+
+		let nbCafeInit = machine.GetNombreCafésServis();
+		let argentInit = machine.GetArgentEncaisse();
+
+		machine.AppuiBoutonAllonge();
+
+		//QUAND on met 40cts
+		machine.Insertion(0.4);
+
+		//ALORS une boisson coule
+		const nbCafeFinal = machine.GetNombreCafésServis();
+		expect(nbCafeFinal).toBe(nbCafeInit + 1);
+
+		//ET 40cts est encaissé
+		const argentFinal = machine.GetArgentEncaisse();
+		expect(argentFinal).toBe(argentInit + 0.4);
+	});
+
+	test("Machine bouton allonge, plus de café, bien payé", () => {
+		//ETANT DONNE une machine à café où on appuie sur le bouton allonge qui n'a plus de café
+		const machine = new Machine();
+		machine.AyantDosesDeCafe(0);
+		let nbCafeInit = machine.GetNombreCafésServis();
+		let argentInit = machine.GetArgentEncaisse();
+		machine.AppuiBoutonAllonge();
+
+		//QUAND on met 40cts
+		machine.Insertion(0.4);
+
+		//ALORS une boisson ne coule pas
+		const nbCafeFinal = machine.GetNombreCafésServis();
+		expect(nbCafeFinal).toBe(nbCafeInit);
+
+		//ET l'argent n'est pas encaissé
+		const argentFinal = machine.GetArgentEncaisse();
+		expect(argentFinal).toBe(argentInit);
+	});
+
+	test("Machine bouton allonge, plus d'eau, bien payé", () => {
+		//ETANT DONNE une machine à café où on appuie sur le bouton allonge qui n'a plus d'eau
+		const machine = new Machine();
+		machine.CouperEau();
+		let nbCafeInit = machine.GetNombreCafésServis();
+		let argentInit = machine.GetArgentEncaisse();
+		machine.AppuiBoutonAllonge();
+
+		//QUAND on met 40cts
+		machine.Insertion(0.4);
+
+		//ALORS une boisson ne coule pas
+		const nbCafeFinal = machine.GetNombreCafésServis();
+		expect(nbCafeFinal).toBe(nbCafeInit);
+
+		//ET l'argent n'est pas encaissé
+		const argentFinal = machine.GetArgentEncaisse();
+		expect(argentFinal).toBe(argentInit);
+	});
+
+	test("Machine bouton allonge, pas assez de sous", () => {
+		//ETANT DONNE une machine à café où on appuie sur le bouton allonge
+		const machine = new Machine();
+		let nbCafeInit = machine.GetNombreCafésServis();
+		let argentInit = machine.GetArgentEncaisse();
+		machine.AppuiBoutonAllonge();
+
+		//QUAND on met 39cts
+		machine.Insertion(0.39);
+
+		//ALORS une boisson ne coule pas
+		const nbCafeFinal = machine.GetNombreCafésServis();
+		expect(nbCafeFinal).toBe(nbCafeInit);
+
+		//ET l'argent n'est pas encaissé
+		const argentFinal = machine.GetArgentEncaisse();
+		expect(argentFinal).toBe(argentInit);
+	});
+});
